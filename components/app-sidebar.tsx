936fbf75b211adc673809297aba1b3f387491267
Amanda Djo <amandadjouob@yahoo.fr>
2026-02-26 19:50:53 +0100

    feat: add stats dashboard with mock chart

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -52,14 +52,14 @@ const data = {
   ],
   navMain: [
     {
-      title: "Playground",
-      url: "#",
+      title: "Backoffice",
+      url: "/",
       icon: SquareTerminal,
       isActive: true,
       items: [
         {
-          title: "History",
-          url: "#",
+          title: "Stats",
+          url: "/dashboard/stats",
         },
         {
           title: "Starred",
